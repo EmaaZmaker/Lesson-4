20383b22a16d71c6ed3e0fb93ce8f333d4a6612f
EmaaZmaker <emaazbinammar@gmail.com>
2025-02-15 11:40:42 +0400

    new1

---
+++ activity2.py
@@ -0,0 +1,5 @@
+notes=int(input("enter you're amount"))
+note100=notes//100
+note50=(notes%100)//50
+print("total number of 100 ruppes note", note100)
+print("total number of 50 ruppes note", note50)
